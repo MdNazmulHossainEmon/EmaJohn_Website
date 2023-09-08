e906ea1443aa7c5c2979ab625e0f8f55b814daf8
Md Nazmul Hossain Emon <imon7810@gmail.com>
2023-09-08 17:40:34 +0600

    added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,15 +5,29 @@ import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import "./Cart.css"
 
 const Cart = ({ cart }) => {
+    // console.log(cart);
+
+    let totalPrice = 0
+    let totalShipping = 0
+
+    for (const product of cart) {
+        // console.log("price: ",product.price);
+        totalPrice = totalPrice + product.price
+        totalShipping = totalShipping + product.shipping
+    }
+
+    const tax = (totalPrice * 7) / 100
+    const grandTotal = totalPrice + totalShipping + tax
+
     return (
         <div className='cart'>
             <h4 className='text-center mb-5'>Order Summary</h4>
             <p>Selected Items: {cart.length} </p>
-            <p>Total Price: $ </p>
-            <p>Total Shipping Charge: $ </p>
-            <p>Tax: $ </p>
+            <p>Total Price: ${totalPrice} </p>
+            <p>Total Shipping Charge: ${totalShipping} </p>
+            <p>Tax: ${tax} </p>
             <p className='mb-5'>
-                <strong>Grand Total: $</strong>
+                <strong>Grand Total: ${grandTotal}</strong>
             </p>
 
             <Button variant='danger'>
